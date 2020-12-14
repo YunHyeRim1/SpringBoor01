8803377c4ccf75dac6c3f5a76e5a5d3957713258
YunHyeRim1 <0305_08@naver.com>
2020-12-14 14:04:55 +0900

    14

---
+++ src/main/java/com/example/demo/sts/service/model/GradeDto.java
@@ -0,0 +1,12 @@
+package com.example.demo.sts.service.model;
+
+import com.example.demo.uss.service.model.StudentDto;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component @Data
+public class GradeDto {
+    
+}
---
+++ src/main/java/com/example/demo/uss/service/impls/StudentServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example.demo.uss.service.impls;
+
+import com.example.demo.uss.service.StudentService;
+
+import org.springframework.stereotype.Service;
+@Service
+public class StudentServiceImpl implements StudentService {
+    
+}
